252a0fbe6c61d9308909a233c4c738e408e7c937
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-13 18:42:22 -0400

    Refactored code

--- main.go
+++ main.go
@@ -5,35 +5,53 @@ import (
 	"os/exec"
 )
 
-func main() {
+func checkCommand(command string) error {
 	_, err := exec.LookPath("xclip")
 	if err != nil {
-		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
+
+func Write(text string) error {
+	err := checkCommand("xclip")
+	if err != nil {
+		return err
 	}
 
 	cmd := exec.Command("xclip", "-selection", "clipboard")
 
 	in, err := cmd.StdinPipe()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = cmd.Start()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	in.Write([]byte("dqasda"))
+	in.Write([]byte(text))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = in.Close()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	err := Write("dawdawdwafajfghkadsjklhfgasldjkghlasjkdh")
 	if err != nil {
 		log.Fatal(err)
 	}
