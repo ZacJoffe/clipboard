00cf850c4e2c5b5e3ed0f229f84e91f0ec1015c1
ZacJoffe <zacharyjoffe@gmail.com>
2019-04-13 18:00:41 -0400

    Got xclip copying working

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"os"
+	"log"
+	"os/exec"
 )
 
 func main() {
+	_, err := exec.LookPath("xclip")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	cmd := exec.Command("xclip", "-selection", "clipboard")
+
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	in.Write([]byte("dqasda"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = in.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
